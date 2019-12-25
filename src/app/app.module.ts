76fbf85496124bfc3a92a521cc7f45cc4fc9a808
Barak Finnegan <finnegansbear1@yahoo.com>
2019-12-25 16:35:33 +0100

    removed unnessasary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { ResumeComponent } from './components/resume/resume.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { ProjectInfoComponent } from './components/project-info/project-info.component';
-import { ProjectListComponent } from './components/project-list/project-list.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { AchievementsComponent } from './components/achievements/achievements.component'
 
 @NgModule({
   declarations: [
