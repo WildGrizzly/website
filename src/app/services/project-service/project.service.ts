a853234ecf1174fbf2d21cffd9fbec40c25a781b
Barak Finnegan <finnegansbear1@yahoo.com>
2019-12-28 16:06:49 +0100

    editing the project to fully accept json file

---
+++ src/app/services/project-service/project.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http' 
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+  subjects;
+  constructor(private http: HttpClient) { 
+    this.getJSON().subscribe(data => {
+      this.subjects = data.Subjects;
+    });
+  }
+
+  public getJSON(): Observable<any> {
+    return this.http.get('../../../assets/subjects.json');
+  }
+
+  public getSubjects() {
+    return this.subjects;
+  }
+}
