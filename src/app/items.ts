9c0d13893a2916b286fbb40d5687fe6707e25f88
wildg <BJFinn98@vt.edu>
2020-01-19 10:15:30 +0100

    change headers to angular material buttons

--- src/app/items.ts
+++ src/app/items.ts
@@ -1,5 +1,18 @@
-export const items: string[] =  [
-    "Projects",
-    "Resumé",
-    "Achievements"
+export const items: {title: string, route: string}[] =  [
+    {
+        title: "Home",
+        route: "/"
+    },
+    {
+        title: "Projects",
+        route: "projects"
+    },
+    {
+        title: "Resume",
+        route: "resumé"
+    },
+    {
+        title: "Achievements",
+        route: "achievements"
+    }
 ]
